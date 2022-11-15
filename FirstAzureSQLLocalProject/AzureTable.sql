1655014464d7636d0579adba9e55d3f0b17dd51e
Javier Montero <jmontero19@gmail.com>
2022-11-14 23:25:32 -0600

    Updating Test

--- FirstAzureSQLLocalProject/AzureTable.sql
+++ FirstAzureSQLLocalProject/AzureTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE [dbo].[AzureTable]
 (
   [Id] INT NOT NULL PRIMARY KEY,
   [CustomerName] VARCHAR(25) not NULL,
-  [DOB] DATE NOT NULL
+  [DOB] DATE NOT NULL,
+  [$_InertedDate] DATETIME2(3) NOT NULL
 )
